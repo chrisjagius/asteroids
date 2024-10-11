aed15d9b41755c2649f75dbd6558e3baadbc867d
chrisjagius <chrisagius@fastmail.com>
2024-10-10 22:06:29 -0400

    fix: change fill color to string for ease of reading

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill("black")
 
         for sprite in updatable:
             sprite.update(dt)
